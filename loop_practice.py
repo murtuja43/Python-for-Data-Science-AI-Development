d3033f3056241ea71904a33bf912dff78236fa84
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-03-02 16:25:42 +0500

    Update loop_practice.py

--- loop_practice.py
+++ loop_practice.py
@@ -2,3 +2,22 @@ list_1 = ["Kayes", "Saif", "Mursalin", "Jim"]
 
 for list in list_1:
     print(list)
+
+# --------------------
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+for color in colors:
+    print(color)
+
+# ---------------------
+
+for number in range(1, 11):
+    print(number)
+
+# ----------------------
+
+for number in range(11):
+    print(number)
+
+# ----------------------
+
