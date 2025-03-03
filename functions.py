4abf9707ee6957b0885f6ed04b5650bc78fc5ed9
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-03-03 21:25:01 +0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,9 @@
+def function_name():
+    pass                    #if we don't want to give any condition of functionality in our function we use 'pass'
+
+# ------------------------
+
+
 def calculate_total(a, b):  # Parameters: a and b
     total = a**2 + b**2           # Task: Addition
     return total            # Output: Sum of a and b
@@ -12,18 +18,25 @@ def number_addition(first_number, second_number, third_number):
     total = first_number + second_number + third_number
     return total
 
-sum = number_addition(34, 4, 2)
-print(sum)
+addnumber = number_addition(34, 4, 2)
+print(addnumber)
 
 
 # ------------------------
 
 
 a = [2, 45, 23, 623,234, 67, 83, 234, 741]
-print(max(a), min(a))
+print(max(a), min(a), sum(a))
 
 
 # ------------------------
+
+def greet(name):
+    greetings = ("Hello " + name)
+    return greetings
+
+greet_them = greet(input("Your name: "))
+print(greet_them)
 
 
 
