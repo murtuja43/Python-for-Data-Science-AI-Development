40d0c4194845fac02f409e3fa5d1f5c960985d06
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-03-03 21:04:52 +0500

    added functions practice file

---
+++ functions.py
@@ -0,0 +1,19 @@
+def calculate_total(a, b):  # Parameters: a and b
+    total = a**2 + b**2           # Task: Addition
+    return total            # Output: Sum of a and b
+
+result = calculate_total(5, 7)  # Calling the function with inputs 5 and 7
+print(result)  # Output: 12
+
+
+# ------------------------
+
+def number_addition(first_number, second_number, third_number):
+    total = first_number + second_number + third_number
+    return total
+
+sum = number_addition(34, 4, 2)
+print(sum)
+
+
+# ------------------------
