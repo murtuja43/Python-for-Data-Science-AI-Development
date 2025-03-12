0c406ab5a377c98fc4f7c4842680b1927c4b22c2
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-03-13 02:15:34 +0500

    Add files via upload

---
+++ objects_&_classes.py
@@ -0,0 +1,13 @@
+class Car:
+    speed = 50
+
+    def __init__(self, speed, start_seconds):
+        self.speed = 50
+        self.start_seconds = 5
+
+    def method1(self, parameter1, parameter2):
+        pass
+    
+    def method2(self, parameter1, parameter2):
+        pass
+
