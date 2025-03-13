9cf633743d0d2fdf69ad1b3ff06af37d2445995b
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-03-13 14:04:03 +0500

    Update objects_&_classes.py

--- objects_&_classes.py
+++ objects_&_classes.py
@@ -9,4 +9,30 @@ r1.age = 3
 r1.color = "red"
 r1.weight = 10
 
+r2 = Robot()
+r2.name = "Jerry"
+r2.age = 2
+r2.color = "orange"
+r2.weight = 1
+
 r1.introduce_self()
+r2.introduce_self()
+
+# -----------------------another method (We will use this new one)
+
+class Robot:
+    def __init__(self, name, age, color, weight):
+        self.name = name
+        self.age = age
+        self.color = color
+        self.weight = weight
+
+    def introduce_self(self):
+        print("My name is ", self.name)
+        print(f"I am {self.age} years old")
+
+r1 = Robot("Tom", 3, "Red", 8)
+r2 = Robot("Jerry", 1, "Orange", 1)
+
+r1.introduce_self()
+r2.introduce_self()
