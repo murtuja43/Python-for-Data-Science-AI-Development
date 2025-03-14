9883147bbc1dbd638038304ceb0a566b51387486
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-03-14 15:39:35 +0500

    Update reading_file.py

--- reading_file.py
+++ reading_file.py
@@ -1,3 +1,13 @@
 with open("file1.txt", "r") as file1:
+    read_file = file1.read()
+    print(read_file)
+
+# -------------------
+
+with open("file1.txt", "r") as file1:
+    read_file = file1.readline()
+    print(read_file)
+    read_file = file1.readline()
+    print(read_file)
     read_file = file1.readline()
     print(read_file)
