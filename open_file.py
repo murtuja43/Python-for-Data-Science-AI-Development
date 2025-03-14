6ce132184ce2b1a93e628cad3e07e0093e18764f
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-03-14 16:11:29 +0500

    Update and rename reading_file.py to open_file.py

--- open_file.py
+++ open_file.py
@@ -11,3 +11,18 @@ with open("file1.txt", "r") as file1:
     print(read_file)
     read_file = file1.readline()
     print(read_file)
+
+
+# ---------------------
+with open("file1.txt", "r") as file1:
+
+    read_line1 = file1.readline()
+
+    read_line2 = file1.readline()
+
+    read_line3 = file1.readline()
+
+    
+print(read_line1)
+print(read_line2)
+print(read_line3)
