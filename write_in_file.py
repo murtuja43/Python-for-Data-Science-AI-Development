79cd5db0709258b5dcca027d4239609356e6402a
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2025-03-16 14:52:20 +0500

    Update write_in_file.py

--- write_in_file.py
+++ write_in_file.py
@@ -5,7 +5,7 @@ with open("new_file2.txt", "w") as Write_file:
 
 
 
-new_data = "This is line 3.1"
+new_data = "This is line 3.5"
 with open("new_file2.txt", "a") as Write_file2:   #use "a" to add (append) new data in the write file.
     Write_file2.write(new_data + "\n")
 
@@ -28,6 +28,13 @@ new_data2 = "This is line 7"
 new_data3 = "This is line 8"
 with open("new_file3.txt", "a") as append_file:
     append_file.write(new_data2 + "\n" + new_data3 + "\n")
+
+
+# add dictionary in the append file
+new_data_dict = ["This is line 9 \n", "This is line 10 \n", "This is line 11 \n"]
+with open("new_file3.txt", "a") as append_file:
+    for line in new_data_dict:
+        append_file.write(line)
 
 
 
